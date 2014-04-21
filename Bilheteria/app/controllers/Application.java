d12e5120aebb7e5ff99fade5cd44e83e8d1ade62
kalinegreiros <kaline.negreiros@gmail.com>
2014-04-21 10:58:09 -0300

    Solução paliativa para o problema da data

--- Bilheteria/app/controllers/Application.java
+++ Bilheteria/app/controllers/Application.java
@@ -5,6 +5,8 @@ import play.mvc.*;
 import play.data.*;
 import play.data.validation.*;
 
+import java.sql.Time;
+import java.sql.Date;
 import java.util.*;
 
 import models.*;
@@ -14,19 +16,19 @@ public class Application extends Controller {
 //Desde que exista um html com o mesmo nome
     public static void index() {
     	List<Evento> eve = Evento.all().fetch();
-    	eve.add(new Evento("Teste alternativo"));
+    	eve.add(new Evento("Teste alternativo", 1, 0, 0, null));
     	Logger.info("Resutado: " + eve.size());
     	System.out.println(eve.size());
         render(eve);
     }
     
-    public static void cadastrarEvento(@Required String nome){
+    public static void cadastrarEvento(@Required String nome, @Required int dia, @Required int mes, @Required int ano, @Required Time hora){
     	
-    	Evento evento = new Evento(nome);
+    	Evento evento = new Evento(nome, dia, mes, ano, hora);
 		/*if (validation.hasErrors()) {
 			render("Application/inserir.html", evento);
 		}*///Quando tiver validação faz algo assim
-
+    	
 		evento.save();
 		index();
     }
--- Bilheteria/app/models/Evento.java
+++ Bilheteria/app/models/Evento.java
@@ -1,6 +1,10 @@
 package models;
 
+import java.sql.Date;
+import java.sql.Time;
+
 import javax.persistence.Entity;//Importa a anotação Entity (que não sei pra quê serve)
+
 import play.*; //Coloquei pra caso precise
 import play.db.jpa.Model; //Classe modelo
 
@@ -10,13 +14,22 @@ public class Evento extends Model {
 	//Por enquanto só o nome
 	private static int nEvento = 0;
 	private String nome;
+	private int dia;
+	private int mes;
+	private int ano;
+	private Time hora;
+	
 	public Evento(){
 		setNome("Evento" + nEvento);
 		nEvento++;
 	}
 	
-	public Evento(String nome){
+	public Evento(String nome, int dia, int mes, int ano, Time hora){
 		this.setNome(nome);
+		this.setDia(dia);
+		this.setMes(mes);
+		this.setAno(ano);
+		this.setHora(hora);
 	}
 
 	public String getNome() {
@@ -26,5 +39,38 @@ public class Evento extends Model {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+		
+	public int getDia() {
+		return dia;
+	}
+
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+
+	public int getMes() {
+		return mes;
+	}
+
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public Time getHora() {
+		return hora;
+	}
+
+	public void setHora(Time time) {
+		this.hora = time;
+	}
+
 	
 }
